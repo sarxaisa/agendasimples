8c212f9868d8a1d8bac126d49aa707f48a7ead16
sarxaisa <118470659+sarxaisa@users.noreply.github.com>
2023-05-04 15:27:35 -0300

    Atualizado
    Atualizei o Projeto

---
+++ AgendaSimples/Contato.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaSimples
+{
+    internal class Contato
+    {
+        private string primeiroNome;
+        private string sobrenome;
+        private string telefone;
+
+        public string PrimeiroNome
+        {
+            get { return primeiroNome; }
+            set { primeiroNome = value; }
+        }
+    
+
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+        public string Telefone
+        {
+            get { return telefone; }
+            set
+            {
+                if (value.Length == 11)
+                    Telefone = value;
+                else
+                    Telefone = "00-00000-0000";
+            }
+        }
+        public Contato()
+        {
+            PrimeiroNome = "José";
+            Sobrenome = "Da Silva";
+            Telefone = " 11-91234-5678";
+        }
+        public Contato (string primeiroNome, string sobrenome, string telefone)
+        {
+            PrimeiroNome = primeiroNome;
+            Sobrenome = sobrenome; 
+            Telefone = telefone;
+
+        }
+        public override string ToString()
+        {
+            string saída = string.Empty;
+        }
+    }
+}
+
+     
